0af5520f7657c2347e3cbad066eefeea95ad9c35
kevin o sullivan <kevinosullivan25@gmail.com>
2022-04-25 23:24:14 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const events = [
+    {title: "Romeo and Juliet",location: "Cork",date: "20/05/2022",capacity: 5000,price: 23.50},
+    {title: "Little Red Riding Hood", location: "Cork", date: "05/05/2022", capacity: 2000,price: 15.50},
+    {title: "U2",location: "Cork",date: "06/06/2022",capacity: 60000,price: 65.00},
+    {title: "Bon Jovie",location: "Galway",date: "07/07/2022",capacity: 70000,price: 70.00},
+    {title: "Ed Sheeran",location: "Galway",date: "08/08/2022",capacity: 55000,peice: 75.00},
+    {title: "Queen",location: "Dublin",date: "08/06/2022",capacity: 69000,price: 61.00},
+    {title: "Slayer",location: "Dublin",date: "08/04/2022",capacity: 62000,price: 58.00},
+    {title: "Britney Spears",location: "Dublin",date: "08/04/2022",capacity: 54000,price: 58.00},
+    {title: "Jayz",location: "Dublin",date: "08/04/2022",capacity: 78000,price: 48.00},
+    {title: "The Wanted",location: "Dublin",date: "08/04/2022",capacity: 34000,price: 78.00}
+]
+
